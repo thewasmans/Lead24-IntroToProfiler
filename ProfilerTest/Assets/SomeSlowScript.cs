c362fc7eef1105c8fa1a6e3fe091e6728f877b52
ajopart-ajcreative <101755934+ajopart-ajcreative@users.noreply.github.com>
2024-10-13 22:00:51 +0200

    first commit

---
+++ ProfilerTest/Assets/SomeSlowScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class SomeSlowScript : MonoBehaviour
+{
+    [SerializeField] private int _iterationValue01;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SomeHeavyCalculation();
+    }
+
+
+    private void SomeHeavyCalculation()
+    {
+        FindPrimeNumber(_iterationValue01);
+    }
+
+    private static long FindPrimeNumber(int n)
+    {
+        int count = 0;
+        long a = 2;
+        while (count < n)
+        {
+            long b = 2;
+            int prime = 1;// to check if found a prime
+            while (b * b <= a)
+            {
+                if (a % b == 0)
+                {
+                    prime = 0;
+                    break;
+                }
+                b++;
+            }
+            if (prime > 0)
+            {
+                count++;
+            }
+            a++;
+        }
+        return (--a);
+    }
+}
